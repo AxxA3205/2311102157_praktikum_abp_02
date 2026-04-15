ad8e4d2226a108d552bd53d3c7b93f9888bbbb4b
Mahija Danadyaksa Sadtomo <91940569+AxxA3205@users.noreply.github.com>
2026-04-15 07:24:37 +0700

    Add files via upload

---
+++ PRAKTIKUM4/data.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json');
+
+$profil = [
+    'nama'      => 'Budi',
+    'pekerjaan' => 'Web Developer',
+    'lokasi'    => 'Jakarta'
+];
+
+echo json_encode($profil);
